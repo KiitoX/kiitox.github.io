efc72488e354b6b019bf425b1e131fda543ec90f
MCManuelLP <mcmanuellp@gmail.com>
2015-12-07 13:54:57 +0100

    bookmarklet fix
    now it doesn't create tons of script tags referencing to the same script
    over and over if you open and close the lyrics thing multiple times
    without reloading

--- lyrics/js-bookmark-applet.js
+++ lyrics/js-bookmark-applet.js
@@ -1,19 +1,17 @@
-var script;
 if (typeof window.lyricsScript === 'undefined') {
-    script = document.createElement('script');
-    script.setAttribute('id', 'lyricsScript');
+    var script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('charset', 'UTF-8');
     script.setAttribute('async', 'true');
     script.setAttribute('src', 'https://mcmanuellp.github.io/lyrics/lyricscrawler.js');
     document.documentElement.appendChild(script);
+    script.onload = script.onreadystatechange = function () {
+        var rs = script.readyState;
+        if (!rs || rs === 'loaded' || rs === 'complete') {
+            script.onload = script.onreadystatechange = null;
+            init();
+        }
+    };
 } else {
-    script = document.getElementById('lyricsScript');
+    init();
 }
-script.onload = script.onreadystatechange = function () {
-    var rs = script.readyState;
-    if (!rs || rs === 'loaded' || rs === 'complete') {
-        script.onload = script.onreadystatechange = null;
-        init();
-    }
-};
