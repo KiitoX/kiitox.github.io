e7fa4818caab0d2cf7fa0513baa40c291be446c8
MCManuelLP <mcmanuellp@gmail.com>
2015-12-27 17:49:23 +0100

    Update js-bookmark-applet.js

--- lyrics/js-bookmark-applet.js
+++ lyrics/js-bookmark-applet.js
@@ -1,18 +1,17 @@
-function addScript(srcUrl) {
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('charset', 'UTF-8');
-    script.setAttribute('async', 'true');
-    script.setAttribute('src', srcUrl);
-    document.documentElement.appendChild(script);
-    return script;
-}
-
 if (typeof window.lyricsScript === 'undefined') {
-    addScript('https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
-    //addScript('js/jquery.ajax-cross-origin.min.js');
-    addScript('https://raw.githubusercontent.com/padolsey-archive/jquery.fn/master/cross-domain-ajax/jquery.xdomainajax.js');
-    var script = addScript('https://mcmanuellp.github.io/lyrics/lyricscrawler.js');
+    var script,
+        scriptUrls = {'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+                    //'js/jquery.ajax-cross-origin.min.js'),
+                      'https://raw.githubusercontent.com/padolsey-archive/jquery.fn/master/cross-domain-ajax/jquery.xdomainajax.js',
+                      'https://mcmanuellp.github.io/lyrics/lyricscrawler.js'};//has to be last...
+    for (i = 0; i < scriptUrls.length; i++) {
+        script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('charset', 'UTF-8');
+        script.setAttribute('async', 'true');
+        script.setAttribute('src', scriptUrls[i]);
+        document.documentElement.appendChild(script);
+    }
     script.onload = script.onreadystatechange = function () {
         var rs = script.readyState;
         if (!rs || rs === 'loaded' || rs === 'complete') {
