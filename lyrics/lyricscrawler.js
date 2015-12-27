25bcda30429af68676ed8bdc3a589a1985c1f46a
MCManuelLP <mcmanuellp@gmail.com>
2015-12-27 18:37:36 +0100

    Update lyricscrawler.js

--- lyrics/lyricscrawler.js
+++ lyrics/lyricscrawler.js
@@ -45,6 +45,7 @@ function lyricsFind() {
 function lyricsLoad() {
     alert("loading");
     $("#invislyr").load(document.getElementById('lyrixdisplay').getAttribute('src'));
+    $("#invislyr").load('http://mcmanuellp.github.io/lyrics/temp.html');
     document.getElementById('lyrics-html').style.fontSize = '0.8em';
     alert(document.getElementById('lyrics-html').innerHtml)
 };
