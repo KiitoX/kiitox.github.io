97f3bce90b5629b18e3a5684c5fc86e8e260e549
MCManuelLP <mcmanuellp@gmail.com>
2015-12-27 17:38:45 +0100

    Update js-bookmark-applet.js

--- lyrics/js-bookmark-applet.js
+++ lyrics/js-bookmark-applet.js
@@ -10,7 +10,7 @@ function addScript(srcUrl) {
 
 if (typeof window.lyricsScript === 'undefined') {
     addScript('https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
-    addScript('js/jquery.ajax-cross-origin.min.js');
+    //addScript('js/jquery.ajax-cross-origin.min.js');
     addScript('https://raw.githubusercontent.com/padolsey-archive/jquery.fn/master/cross-domain-ajax/jquery.xdomainajax.js');
     var script = addScript('https://mcmanuellp.github.io/lyrics/lyricscrawler.js');
     script.onload = script.onreadystatechange = function () {
