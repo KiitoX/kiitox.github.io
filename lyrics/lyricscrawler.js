6e926c11e95820a16c81cd46b7c31042bbd0a064
MCManuelLP <mcmanuellp@gmail.com>
2015-12-27 19:04:23 +0100

    Update lyricscrawler.js

--- lyrics/lyricscrawler.js
+++ lyrics/lyricscrawler.js
@@ -1,38 +1,39 @@
-var lyricsScript = true,
-    parentId = 'nav',
-    childId = 'lyrics',
-    parent = document.getElementById(parentId),
-    child = document.createElement('div');
+var lyricsScript = true;
 
 function init() {
-    displayPopup();
-}
-
-function removePopup() {
-    parent.removeChild(child);
-}
-
-function setContent() {
-    var objXml = new XMLHttpRequest();
-    objXml.onreadystatechange = function () {
-        if (objXml.readyState === 4) {
-            if(objXml.status === 200) {
-                child.innerHTML = objXml.responseText;
-            } else {
-                child.innerHTML = "<p style='text-align:center;color:lightcoral;margin:20px;'>Couldn't load page content.</p>";
+    var parentId = 'nav',
+        childId = 'lyrics',
+        parent = document.getElementById(parentId),
+        child = document.createElement('div');
+        
+    function removePopup() {
+        parent.removeChild(child);
+    }
+    
+    function setContent() {
+        var objXml = new XMLHttpRequest();
+        objXml.onreadystatechange = function () {
+            if (objXml.readyState === 4) {
+                if(objXml.status === 200) {
+                    child.innerHTML = objXml.responseText;
+                } else {
+                    child.innerHTML = "<p style='text-align:center;color:lightcoral;margin:20px;'>Couldn't load page content.</p>";
+                }
             }
-        }
-    };
-    objXml.open("GET", 'https://mcmanuellp.github.io/lyrics/lyrics.html', true);
-    objXml.send();
-}
-
-function displayPopup() {
-    child.setAttribute('id', childId);// style: position:fixed;bottom:50px;right:0;
-    child.style.width = 'inherit';
-    child.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-    parent.appendChild(child);
-    setContent();
+        };
+        objXml.open("GET", 'https://mcmanuellp.github.io/lyrics/lyrics.html', true);
+        objXml.send();
+    }
+    
+    function displayPopup() {
+        child.setAttribute('id', childId);// style: position:fixed;bottom:50px;right:0;
+        child.style.width = 'inherit';
+        child.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+        parent.appendChild(child);
+        setContent();
+    }
+    
+    displayPopup();
 }
 
 function lyricsScript(url) {
